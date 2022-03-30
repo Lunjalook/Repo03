854f9f918bc23c0219971ce0932769e001422209
Lunjalook <azra.cosickic@gmail.com>
2022-03-30 14:08:58 +0200

    Adding project 06

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,8 +1,8 @@
 function main() {
     // Only change code below this line
-    var x;
-    var y;
-    var z;
+    var x = 7;
+    var y = 12;
+    var z = "My name is";
     // Only change code above this line
     x = x + 2;
     y = y + 4;
