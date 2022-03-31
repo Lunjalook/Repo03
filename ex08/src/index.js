267e69cba24fb765a432c6124aa72460fb5fec61
Lunjalook <azra.cosickic@gmail.com>
2022-03-31 06:59:29 +0200

    Adding project 08

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,8 +1,6 @@
 function main(numOne, numTwo) {
     //Only change code below this line
     var sum;
-    var numOne;
-    var numTwo;
     sum = numOne + numTwo;
     //Only change code above this line
     return sum;
