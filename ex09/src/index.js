0410c1c3ed3264eadf83ac97dfe63a6f6e3de8c6
Lunjalook <azra.cosickic@gmail.com>
2022-03-31 07:00:55 +0200

    Adding project 09

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,8 +1,6 @@
 function main(numOne, numTwo) {
     //Only change code below this line
     var difference;
-    var numOne;
-    var numTwo;
     difference = numOne - numTwo;
     //Only change code above this line
     return difference;
